3d59c04547dd60bfd6c6ac4f2774a19475dd8b46
Liang@Mag+ <liang@magplus.com>
2016-01-25 20:51:10 +0100

    update my list

--- list2priority copy.rb
+++ list2priority copy.rb
@@ -4,7 +4,7 @@ puts "Welcome to Liang's List!"
 puts "\nYou are now about to input things that you need to do."
 puts "For each item, you will be asked if it is important and if it is urgent."
 
-puts "\nPlease anwser yes (y) or no (n), and press 'Enter' to quit."
+puts "\nPlease anwser yes or no, and press 'Enter' to quit."
 puts "*" * 50
 
 ImportantAndUrgent = Array.new
@@ -15,17 +15,16 @@ NotImportantNotUrgent = Array.new
 all_list = [ImportantAndUrgent, ImportantButNotUrgent, UrgentButNotImportant, NotImportantNotUrgent]
 # all_list = Array.new
 
-print "Plase add your to-do: "
+puts "Plase add your to-do: "
 todo = gets.chomp
 
 while todo.downcase != ""
-  print "Is this important? "
+  puts "Is this important?"
   important = gets.chomp
-  
   if important == ""
     break
   elsif important == "yes" or important == "y"
-    print "Is this urgent? "
+    puts "Is this urgent?"
     urgent = gets.chomp
     
     if urgent == "yes" or urgent == "y"
@@ -36,7 +35,7 @@ while todo.downcase != ""
       ImportantButNotUrgent.push(todo)
     end
   else
-    print "Is this urgent? "
+    puts "Is this urgent?"
     urgent = gets.chomp
     
     if urgent == "yes" or urgent == "y"
@@ -47,48 +46,48 @@ while todo.downcase != ""
       NotImportantNotUrgent.push(todo)
     end
   end
-  print "Please add new task: "
+  puts("Please add new task: ")
   todo = gets.chomp
   
 end
 
 # print out the result
-puts "Your list of things are: "
-puts "*" * 50
+puts "Your list of things are:"
+print "*" * 50
 
 if all_list.flatten.length == 0
-	puts "\nThere is nothing in your list.\n"
+	print "\nThere is nothing in your list.\n"
 else
-	puts "\nHere is all your input"
-	all_list.each{|x| puts x}
+	print "\nHere is all your input"
+	all_list.each{|x| print x}
 	
-	puts "\nHere is your list of priorities\n"
+	print "\nHere is your list of priorities\n"
 
 	if ImportantAndUrgent.length == 0
-		puts "\nThere is nothing in your priority 1 list."
+		print "\nThere is nothing in your priority 1 list."
 	else
-		puts "\nPriority 1: Important and Urgent"
-    ImportantAndUrgent.each{|x| puts x}
+		print "\nPriority 1: Important and Urgent"
+    ImportantAndUrgent.each{|x| print x}
   end
 
 	if ImportantButNotUrgent.length == 0
-		puts "\nThere is nothing in your priority 2 list."
+		print "\nThere is nothing in your priority 2 list."
 	else
-		puts "\nPriority 2: Important but not Urgent"
-		ImportantButNotUrgent.each{|x| puts x}
+		print "\nPriority 2: Important but not Urgent"
+		ImportantButNotUrgent.each{|x| print x}
 	end
 		
 	if UrgentButNotImportant.length == 0
-		puts "\nThere is nothing in your priority 3 list."
+		print "\nThere is nothing in your priority 3 list."
 	else
-		puts "\nPriority 3: Not Important but Urgent"
-		UrgentButNotImportant.each{|x| puts x}
+		print "\nPriority 3: Not Important but Urgent"
+		UrgentButNotImportant.each{|x| print x}
 	end
 
 	if NotImportantNotUrgent.length == 0
-		puts "\nThere is nothing in your priority 4 list."
+		print "\nThere is nothing in your priority 4 list."
 	else
-		puts "\nPriority 4: Not Important but not urgent"
-		NotImportantNotUrgent.each{|x| puts x}
+		print "\nPriority 4: Not Important but not urgent"
+		NotImportantNotUrgent.each{|x| print x}
 	end
 end
